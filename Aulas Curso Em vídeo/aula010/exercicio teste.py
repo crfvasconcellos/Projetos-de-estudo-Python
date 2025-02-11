5ba8266608faf65fc026d57d4b98617959be861c
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2025-02-11 10:07:51 -0300

    mudei o texto

--- Aulas Curso Em vídeo/aula010/exercicio teste.py
+++ Aulas Curso Em vídeo/aula010/exercicio teste.py
@@ -2,4 +2,4 @@ idade= int(input("Quantos anos tem o seu carro? "))
 if idade <=3:
     print("seu carro ainda é novo")
 else:
-    print("ih, ja tá na hora de trocar") 
+    print(" ja tá na hora de trocar")
