a37e3a8db1a9ccf0dac0e24f9731b9dd1c8a7985
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2025-02-11 10:27:34 -0300

    mais um desafio

---
+++ Aulas Curso Em vídeo/aula010/exercicio teste 02.py
@@ -0,0 +1,8 @@
+nota1= float(input("qual a sua nota de mat 1? "))
+nota2= float(input("qual a sua nota de mat 2? "))
+soma= (nota1 + nota2)/2
+print("a sua media foi {}".format(soma))
+if soma <=5.9:
+    print("Você foi reprovado, melhore!")
+else:
+    print("você foi Aprovado, vc é o cara!!")
