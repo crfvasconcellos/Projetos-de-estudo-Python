d7c19c302687fa14f7aaecdca71cbc591afd4942
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2025-02-11 10:48:53 -0300

    desafio 28

---
+++ Aulas Curso Em vídeo/aula010/desafio 28.py
@@ -0,0 +1,12 @@
+import random
+print("Tente adivinhar em qual número eu pensei de 0 a 5!")
+numero = random.randrange(0,6) #escolhendo um número aleatorio
+escolhido= int(input("Qual você acha que é? "))
+if escolhido >6 or escolhido <0 :
+    print('esse não é um dos números elegiveis')
+else:
+    if escolhido== numero :
+        print("Caramba você conseguiu!!!")
+    else:
+        print("afe, reinicie o programa e tente novamente")
+        print('o número era {}'.format(numero))
