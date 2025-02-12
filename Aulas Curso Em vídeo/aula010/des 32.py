19986dd5471d899d150c831b11a11d4a68592f83
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2025-02-12 15:42:08 -0300

    melhorando ano bissexto des 32

---
+++ Aulas Curso Em vídeo/aula010/des 32.py
@@ -0,0 +1,10 @@
+print("Vou analisar se tal ano foi/é Bissexto")
+ano= int(input("ANO: "))
+anobi= (ano % 4)
+
+if ano % 100 == 0 and ano % 400 == 0:
+    print(é bissexto)
+else ano % 400 == 0:
+    print("não é bissexto")
+
+if
