49d17ada4f9332d9ad9269efcb52ce4f69c9e44b
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2025-02-13 11:24:06 -0300

    Update des 34.py

--- Aulas Curso Em vídeo/aula010/des 34.py
+++ Aulas Curso Em vídeo/aula010/des 34.py
@@ -1,11 +1,10 @@
 print("Vamos Calcular o seu aumento!!!")
 sal1= float(input("Quanto voce recebe agora? "))
-sal2= 0
-if sal1 > 1250.0 :
-    sal2 = ((sal1 / 100) * 10) + sal1
-    au= ('10%')
 if sal1 <= 1250.0:
     sal2= ((sal1 / 100 ) * 15) + sal1
     au= ("15%")
+else:
+    sal2 = ((sal1 / 100) * 10) + sal1
+    au= ('10%')
 
 print("Seu novo salário é {} e recebeu um aumentou de {}".format(sal2, au))
