f335dfc889765ca73926f0f92f5a5f86fe99ff9d
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2025-02-13 14:16:54 -0300

    Concertei des 32 mundo 1

--- Aulas Curso Em vídeo/Mundo 1 PYTHON/aula010/des 32.py
+++ Aulas Curso Em vídeo/Mundo 1 PYTHON/aula010/des 32.py
@@ -2,10 +2,8 @@ print("Vou analisar se tal ano foi/é Bissexto")
 ano= int(input("ANO: "))
 anobi= (ano % 4)
 
-if anobi != 0:
-    print('não é bissexto')
-
 if ano % 100 == 0 and ano % 400 == 0:
+<<<<<<< Updated upstream:Aulas Curso Em vídeo/Mundo 1 PYTHON/aula010/des 32.py
     print('é bissexto')
 else:
     if ano % 100 == 0 and anobi == 0:
@@ -14,4 +12,10 @@ else:
     else:
         if anobi == 0:
             print("é  um ano bissexto")
+=======
+    print(é bissexto)
+else ano % 400 == 0:
+    print("não é bissexto")
+>>>>>>> Stashed changes:Aulas Curso Em vídeo/aula010/des 32.py
 
+if
