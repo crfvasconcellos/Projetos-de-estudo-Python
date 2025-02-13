00d70e274a2149a12f491ba16d91c6bf10bc6fee
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2025-02-13 14:14:43 -0300

    pratica aula 12

---
+++ Aulas Curso Em vídeo/Mundo 2 PYTHON/Aula 12/Prática Aula 12.py
@@ -0,0 +1,7 @@
+nome= str(input('Qual o seu nome? ')).lower()
+if nome == 'claudio':
+          print('Que nome lindo, bem vindo, {}.'.format(nome))
+elif nome == 'tulio':
+    print('Voce deve ser bem feio... Bem vindo, {}'.format(nome))
+else:
+    print('Seja bem vindo, estranho, chamado {}'.format(nome))
