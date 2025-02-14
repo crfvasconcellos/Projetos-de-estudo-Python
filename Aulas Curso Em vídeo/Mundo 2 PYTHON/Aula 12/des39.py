57000cd9c0f9c88148e1175d96470879c77f587e
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2025-02-14 13:35:20 -0300

    Update des39.py

--- Aulas Curso Em vídeo/Mundo 2 PYTHON/Aula 12/des39.py
+++ Aulas Curso Em vídeo/Mundo 2 PYTHON/Aula 12/des39.py
@@ -1,7 +1,7 @@
-import datetime
+from datetime import date
 print('Irei analisar quando você deve se alistar')
 ano= int(input('EM QUE ANO VOCÊ NASCEU: '))
-hoje= str(datetime.date.today())
+hoje= str(date.today())
 hoje= int(hoje[:4])
 ida= hoje - ano
 if ida == 18:
@@ -9,8 +9,8 @@ if ida == 18:
 elif ida < 18:
     fal= 18 - ida
     ano2= hoje + fal
-    print("VOCÊ AINDA TEM TEMPO PARA SE ALISTAR, FALTAM {} ANOS\nE VOCÊ SE ALISTARÁ EM {}".format(fal,ano2))
+    print("VOCÊ AINDA TEM TEMPO PARA SE ALISTAR, FALTAM {} ANO(S)\nE VOCÊ SE ALISTARÁ EM {}".format(fal,ano2))
 elif ida > 18:
     fal= ida - 18
     ano2 = hoje - fal
-    print('PASSOU O SEU PRAZO DE SE ALISTAR HÁ {} ANOS\nE VOCÊ SE ALISTOU EM {}'.format(fal, ano2))
+    print('PASSOU O SEU PRAZO DE SE ALISTAR HÁ {} ANO(S)\nE VOCÊ SE ALISTOU EM {}'.format(fal, ano2))
