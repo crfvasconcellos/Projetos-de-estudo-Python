d062274ba54fa0da4258b0b7e727bb539491d0d6
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2025-02-18 19:51:28 -0300

    Create des 44.py

---
+++ Aulas Curso Em vídeo/Mundo 2 PYTHON/Aula 12/des 44.py
@@ -0,0 +1,21 @@
+print('=' * 10, 'LOJA', '='*10)
+pre= float(input('Preços das compras: '))
+print('FORMAS DE PAGAMENTO\n [1] À VISTA DINEHIRO/CHEQUE\n [2] À VISTA CRTÃO \n [3] 2X NO CARTÃO\n [4] 3X OU MAIS NO CARTÃO')
+op= input('Qual é a opção? ')
+
+if op == 1:
+    pre= pre - (0.1 * pre)
+    print('POR PAGAR Á VISTA NO DINHEIRO/CHEQUE, VOCÊ TERÁ 10% DE DESCONTO E PAGARÁ {}'.format(pre))
+elif op == 2:
+    pre= pre - (0.05 * pre)
+    print(f'PELO PAGAMENTO SER À VISTA NO CARTÃO, VOCÊ RECEBERÁ 5% DE DESCONTO, PAGANDO {pre}')
+elif op == 3:
+    par= pre/2
+    print(f'PARCELAREMOS SEU PAGAMENTO DE {pre} EM 2 X DE {par} PELO PREÇO ORIGINAL')
+elif op == 4 :
+    x = int(input('Quantas vezes você deseja parcelar sua compra (3 ou mais): '))
+    if x < 3:
+        print('número de parcelas inválidas pela opção escolhida')
+    else:
+        pre2= (pre + pre * 0.2) / x
+        print('VOCÊ PAGARÁ {} PARCELAS DE  {} COM UM JUROS DE 20%'.format(x,pre2))
