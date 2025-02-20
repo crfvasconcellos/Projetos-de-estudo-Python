b03e9e0a77ad1bc147bda27c1be758aa0d43ea94
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2025-02-20 10:59:12 -0300

    Update des 46.py

--- Aulas Curso Em vídeo/Mundo 2 PYTHON/Aula 13/des 46.py
+++ Aulas Curso Em vídeo/Mundo 2 PYTHON/Aula 13/des 46.py
@@ -4,7 +4,7 @@ d= input('Deseja começar a contagem? (s/n) ').lower().strip()
 if d == 'n':
     print('Programa Encerrado')
 elif d == 's':
-    for a in range(10,0,-1):
+    for a in range(10,-1,-1):
         time.sleep(1)
         print(a)
 print('Os fogos Começaram!!!!!')
