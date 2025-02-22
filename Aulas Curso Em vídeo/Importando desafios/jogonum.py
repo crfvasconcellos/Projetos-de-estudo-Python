4c2a168c6e45eee10e09be5032dd6f838c7462da
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2025-02-22 11:53:09 -0300

    Criei um exe do desafio passado

---
+++ Aulas Curso Em vídeo/Importando desafios/jogonum.py
@@ -0,0 +1,22 @@
+import random
+
+c= 1
+r= ''
+print('''Sou seu Computador...
+Acabei de pensar em um número entre 0 e 10.
+Será que Você consegue adivinhar qual foi?''')
+
+while r != 'n':
+    num= random.randint(1,10)
+    p= int(input('Qual o seu palpite: '))
+    if p == num:
+        print('Parabéns!!! Você acertou de primeira!')
+    elif p != num:
+        while p != num:
+            p= int(input('humm, não é esse, tente novamente! Palpite: '))
+            c= c + 1
+            if p == num:
+                print('Você Conseguiu!!!')
+
+    print('Você demorou {} tentativas para acertar o número pensado({})'.format(c,num))
+    r= input('Deseja Jogar Novamente? [s/n] ').strip().lower()
