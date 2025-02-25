b70ccc53029393476a084bfd266dac2d4036a183
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2025-02-25 11:16:45 -0300

    Update des 60.py

--- Aulas Curso Em vídeo/Mundo 2 PYTHON/Aula 14/des 60.py
+++ Aulas Curso Em vídeo/Mundo 2 PYTHON/Aula 14/des 60.py
@@ -1,13 +1,12 @@
 print('Calculadora de fatorial!')
-num= int(input('Digite um número para calcular seu fatorial: '))
-a= 1
-b= ''
-while b != 'OK':
-        num= num -1
-        a= a * num
-        print(' {} '.format(num), end='x' if num > 1 else '')
+num= int(input('Digite um número para calcular seu fatorial: ')) + 1
+f= 1
+print('{}!= '.format(num - 1), end= '')
+while num > 1:
+    num= num - 1
+    f= f * num
+    print('{}'.format(num),end=' x ' if num> 1 else '')
 
-
-print('=',a)
-print('O Resultado é', a)
+print(' =', f)
+print('O Resultado é {}'.format(f))
 
