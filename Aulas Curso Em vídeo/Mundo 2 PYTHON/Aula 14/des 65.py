88969fa0fa958eec8a196439e68e6b2f3dbb49da
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2025-02-26 11:29:10 -0300

    fiz des 65

---
+++ Aulas Curso Em vídeo/Mundo 2 PYTHON/Aula 14/des 65.py
@@ -0,0 +1,19 @@
+num= med= maior = menor= soma = count= 0
+desejo ='S'
+
+while desejo == 'S':
+    num= int(input('Digite um número: '))
+    soma= soma + num
+    count= count + 1
+    if count == 1:
+        maior = num
+        menor= num
+    if num > maior:
+        maior= num
+    elif num < menor:
+        menor= num
+    desejo= str(input('Quer Continuar? [s/n] ')).upper().strip()
+    if desejo == 'N':
+        med= float(soma/count)
+        
+print('Você digitou {} números e a média foi {}\nO maior valor foi {} e o menor foi {}'.format(count,med,maior,menor))
