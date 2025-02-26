8d7d743214cb204cb2fe4430ad4b4ce5283c8a03
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2025-02-26 11:05:32 -0300

    Fiz des 64

---
+++ Aulas Curso Em vídeo/Mundo 2 PYTHON/Aula 14/des 64.py
@@ -0,0 +1,10 @@
+num= 0
+c= 0
+s= 0
+while num != 999:
+    num= int(input("Digite um número [999 para parar]: "))
+    if num != 999:
+        c= c + 1
+        s= s + num
+
+print('Você digitou {} números e a soma entre eles foi {}'.format(c,s))
