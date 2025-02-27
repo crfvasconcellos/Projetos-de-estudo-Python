c74ad0560ccc6d79924979a32984680e466f0333
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2025-02-27 13:12:55 -0300

    Update des 70.py

--- Aulas Curso Em vídeo/Mundo 2 PYTHON/Aula 15/des 70.py
+++ Aulas Curso Em vídeo/Mundo 2 PYTHON/Aula 15/des 70.py
@@ -1,22 +1,17 @@
 print('='*30)
 print('LOJA DO CLAUDIO!')
 print('='*30)
-desejo=''
-nmenor= ' '
-pmenor= 0
-c1000=0
-totalg=0
-c=0
+
+desejo=nmenor=''
+pmenor=totalg=c1000=c=0
+
 while True:
     produto= str(input('Nome do Produto: '))
     preço= int(input('Preço: R$'))
-    if c==0:
+    if c==0 or preço < pmenor:
         nmenor= produto
         pmenor= preço
     c=c+1
-    if preço < pmenor:
-        nmenor= produto
-        pmenor= preço
     totalg= totalg + preço
 
     if preço>1000:
@@ -29,5 +24,4 @@ while True:
             break
     if desejo == 'N':
         break
-
 print('Programa finalizado!\nO gasto Total foi R${}\n{} produtos custam mais de R$1000\nO produto mais barato é {} custando R${}'.format(float(totalg),c1000,nmenor,pmenor))
